fc3697c2cbffff08f7372bcbcb1cf2a63092b39b
Kirill Liapustin <asaprokki@gmail.com>
2024-12-08 16:36:51 +0500

    Внесены коректировки в циклы

--- Main.cpp
+++ Main.cpp
@@ -29,7 +29,19 @@ void Delete2(char a, const char* arr)
 // Задача 3
 std::string Replacement(std::string str, char ch, size_t pos) 
 {
-    str.insert(pos, 1, ch);
+    if (pos <= str.length()) {
+        std::string result;
+
+        for (size_t i = 0; i < pos; ++i) {
+            result += str[i];
+        }
+        result += ch;
+
+        for (size_t i = pos; i < str.length(); ++i) {
+            result += str[i];
+        }
+        return result;
+    }
     return str;
 }
 
@@ -38,80 +50,97 @@ int main()
     setlocale(LC_ALL, "RUS");
 
     // Задача 1
-    char arr[] = { "\0!%&ih$#@" };
+    char arr[] = { "\0!@#$%^&*" };
     std::cout << "Задача 1\n";
     std::cout << "Дана строка с символами\n";
     Print(arr);
-    std::cout << "Введите номер символа и он удалится\n";
+    std::cout << "\nВведите номер символа и он удалится\n";
     int a;
     std::cin >> a;
     Delete(a, arr);
-    std::cout << "\n";
+    std::cout << "\n\n";
 
     // Задача 2
     std::cout << "Задача 2\n";
     std::cout << "Дана строка с символами\n";
     Print(arr);
-    std::cout << "Введите символ и он удалится\n";
+    std::cout << "\nВведите символ и он удалится\n";
     char b;
     std::cin >> b;
     Delete2(b, arr);
-    std::cout << "\n";
+    std::cout << "\n\n";
 
-    // Задача 3
+     Задача 3
     std::cout << "Задача 3\n";
+    std::cout << "Дана строка с выражением\nHello, world!\n";
     std::string String = "Hello, world!";
-    std::string modifiedString = Replacement(String, 'X', 1);
+    std::cout << "Введите символ и позицию программа вставит в выражение\n";
+    char x;
+    int y;
+    std::cout << "Введите символ\n";
+    std::cin >> x;
+    std::cout << "Ввежите номер позиции\n";
+    std::cin >> y;
+    std::string modifiedString = Replacement(String, x, y);
     std::cout << modifiedString << std::endl;
+    std::cout << "\n\n";
 
-    // Задача 4
+     Задача 4
     std::cout << "Задача 4\n";
     std::cout << "Введите любой текст. Программа заменит все символы точки «.» в строке, на символы восклицательного знака «!».\n";
     std::string text;
-    std::cout << "Введите строку: ";
+    std::cout << "Введите текст: ";
+    std::cin >> text;
     std::getline(std::cin, text);
-    for (char& c : text) 
+    for (int i = 0; i < text.length(); i++) 
     {
-        if (c == '.') {
-            c = '!';
+        if (text[i] == '.') 
+        {
+            text[i] = '!';
         }
     }
     std::cout << "Измененная строка: " << text << std::endl;
+    std::cout << "\n\n";
 
-    // Задача 5
+     Задача 5
     std::cout << "Задача 5\n";
     std::cout << "Введите любой текст и символ. Программа посчитает сколько раз символ встречался в строке.\n";
     std::string text1;
     std::cout << "Введите строку: ";
+    std::cin >> text1;
     std::getline(std::cin, text1);
     std::cout << "Введите символ: ";
     char z;
     std::cin >> z;
     int count{};
-    for (char c : text1)
+    for (int i = 0; i < text1.length(); i++)
     {
-        if (c == z) {
+        if (text1[i] == z)
+        {
             count++;
         }
     }
     std::cout << "Символ " << z << " встречается в тексте " << count << " раз." << std::endl;
+    std::cout << "\n\n";
 
     // Задача 6
     std::cout << "Задача 6\n";
     std::cout << "Введите любой текст. Программа Определит количество букв, количество цифр и количество остальных символов.\n";
     std::string text3;
+    std::cout << "Введите текст: ";
+    std::cin >> text3;
     std::getline(std::cin, text3);
     int Letters = 0;
     int Numbers = 0;
     int OtherChars = 0;
 
-    for (char c : text3) 
+    for (int i = 0; i < text3.length(); ++i)
     {
-        if (isalpha(c)) 
+        if (isalpha(text3[i]))
         {
             Letters++;
         }
-        else if (isdigit(c)) 
+        else if (isdigit(text3[i]))
         {
             Numbers++;
         }
